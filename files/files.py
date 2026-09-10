afac9b62781d1dc145758b0bfaa83ad21e170211
Saif Emad <saifbayoumy@gmail.com>
2026-09-10 21:51:55 +0300

    Clarify clear_empty_folders behavior

--- files/files.py
+++ files/files.py
@@ -947,7 +947,8 @@ def clear_empty_folders(
     """Deletes empty folders from the specified folder.
 
     Only folders that are empty when they are checked are deleted. When
-    recursive is True, folders inside subfolders are also checked.
+    recursive is True, folders inside subfolders are also checked. A folder
+    that becomes empty after it has already been checked will not be deleted.
 
     Args:
         folder_name: The name or path of the folder to search.
